f13a02562c05d5ad040635cc39ffb3179239932d
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-08-06 14:40:29 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class EvenOdd
+{
+ public static void main(String args[])
+ {
+   Scanner a=new Scanner(System.in);
+   int s=a.nextInt();
+   if(s%2==0)
+   {
+     System.out.println("even");
+   }
+   else if(s%2!=0)
+   {
+     System.out.println("odd");
+   }
+   else
+   {
+     System.out.println("invalid");
+   }
+  }
+ } 
