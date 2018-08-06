ac2ac345ef1ece38bc65061dacf4055a615427b4
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-08-06 14:24:13 +0530

    Create Check.java

---
+++ Check.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Check
+{
+ public static void main(String args[])
+ {
+  int a=3;
+  if(a>0)
+  {
+   System.out.println("positive");
+  }
+  else
+  {
+   System.out.println("negative");
+  }
+  }
+ } 
