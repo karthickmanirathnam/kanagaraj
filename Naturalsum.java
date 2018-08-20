1a4baf1d79f5d6aeb7547dc4e34fbb4e8c9a7c31
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-08-20 14:11:38 +0530

    Create Naturalsum.java

---
+++ Naturalsum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Naturalsum
+{
+  public static void main(String args[])
+  {
+   int sum=0;
+    system.out.println("Enter the number:");
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    for(int i=0;i<a;i++)
+    {
+     sum=sum+a;
+     }
+    }
+   } 
