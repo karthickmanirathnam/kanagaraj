2065590ea63e0feec45ba5518b8eea73ac196392
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-08-20 13:59:08 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Leapyear
+{
+  public static void main(String args[])
+  {
+    Scanner a=new Scanner(System.in);
+    int b=a.nextInt();
+    if(b%2==0)
+    {
+      System.out.println("yes");
+    }
+    else
+    {
+       System.out.println("no");
+    }
+   }
+  } 
+       
+     
