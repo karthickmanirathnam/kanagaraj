74bdb2bade59f7e8e46828013ccfd078c29489d4
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-08-27 14:07:13 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Alpha
+{ 
+   public static void main(String args[])
+   {
+     char k;
+     Scanner s=new Scanner(System.in);
+     k=s.next();
+     if(k>="a" && k>="A")
+     {
+       System.out.println("Alphabets");
+     }
+     else
+     {
+       System.out.println("Numeric values");
+     }
+    }
+    }
+      
