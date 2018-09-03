ea5897021eecda2c841d5a96120ae6aa15d2546a
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-09-03 13:46:48 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class Prime
+{
+  public static void main(String args[])
+  {
+    int i,n;
+    boolean flag=false;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    for(i=2;i<=n/2;++i)
+    {
+      if(n%i==0)
+      {
+        flag=true;
+        break;
+       }
+      }
+     if(!flag)
+     {
+       System.out.println("prime");
+      }
+      else
+      {
+        System.out.println("not prime);
+      }
+    }
+  }  
