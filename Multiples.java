0b4d88a871e34a5e903a291889d7879eab78bcb3
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-09-03 12:17:22 +0530

    Create Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Multiples
+{
+  public static void main(String args[])
+  {
+    int i,s;
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    for(i=1;i<=5;i++)
+    {
+      s=a*i;
+      System.out.println(s);
+    }
+   }
+ }  
+       
