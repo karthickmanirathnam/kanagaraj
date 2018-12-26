7bd25a0f82c1022441257ff809606ee24f4e51ed
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2018-12-26 14:31:04 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Odd
+{
+ public static void main(String args[])
+ {
+   int n=0,i=0;
+   Scanner sc= new Scanner(System.in);
+   System.out.println("enter n");
+   n=sc.nextInt();
+   for(i=0;i<n;i++)
+   {
+   if(i%2!=0)
+   System.out.println(i+"");
+   }
+   System.out.println();
+   }
+  } 
+   
+
+   
