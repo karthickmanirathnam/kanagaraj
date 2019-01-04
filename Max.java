f6986f765fa2555e30a3e8b0a5a6d0d0f3ccbcf4
karthickmanirathnam <42136197+karthickmanirathnam@users.noreply.github.com>
2019-01-04 12:05:48 +0530

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+class Max
+{ 
+  public static void main(String args[])
+  {
+  int n,max;
+   int a[]=new int[];
+   Scanner sc=new Scanner(System.in);
+   n=sc.nextInt(); //no.of elements
+   for(int i=0;i<n;i++) //to get user input
+   {
+      a[i]=sc.nextInt();
+   }
+    for(int i=0;i<n;i++)// display user input
+    {
+       System.out.println(a[i]);
+    }
+    max=a[0];
+    for(int i=0;i<n;i++)
+      {
+        if(a[i]>max)
+        {
+          max=a[i];
+         }
+        System.out.println(max);
+      }
+    }
+  }  
+      
+        
+    
+  
+ 
